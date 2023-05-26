e54c8ad9e9c222af760fa1fb1510ff4ec911ef60
landen albin <landen@albinfamily.com>
2023-05-25 22:12:36 -0500

    idk we at this again

--- js.js
+++ js.js
@@ -124,6 +124,7 @@ app.use((err, req, res, next) => {
         res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-        console.log('live on Port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+        console.log(`live on Port ${port}`);
 })
